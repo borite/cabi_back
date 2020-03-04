60e352ff35af77a4518f687556e6f200e0074398
David Zheng <nmdigboy@hotmail.com>
2020-03-04 18:08:22 +0800

    加入添加产品

---
+++ js/request_objs.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+
+//添加产品信息Obj
+var productInfo={
+		"ThemeID":"1",
+		"NewTitle":"新闻的标题",
+		"Price":"999",
+		"Contents":"这里存编辑器的内容",
+		"Discribe":"这是一个描述",
+		"Color":"红色,绿色,蓝色",
+		"TopRecommend":"true",
+		"SizeInfo":"L,XL,M",
+		"Scene":"商务",
+		"ListImg":"列表页显示一张",
+		"ImgList":"详情页Banner图片数组",
+		"CollectionImg":"这里放收藏图",
+		"Desplay":"99",
+		"TopDesplay":"99",
+		"AllDesplay":"99",
+		"IsLocked":"false",
+		"Remark":"这里是后台的备注"
+}
